e7ff6bed4eba9979b25cffa59a8a72d6d56e6334
bhashimoto <brunothashimoto@gmail.com>
2024-07-10 15:46:54 -0300

    fix for integration

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+#!/usr/bin/python3
+import sys
+import logging
+import json
 import cv2 as cv
 from qreader import QReader
 from datetime import datetime
 
+logger = logging.getLogger(__name__)
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     last_capture = datetime(2000,1,1)
     cap = cv.VideoCapture(0)
     cap.set(cv.CAP_PROP_FPS,30)
     qreader = QReader(model_size='n', min_confidence=0.8)
 
     if not cap.isOpened():
-        print("Cannot open camera")
+        logger.info("Cannot open camera")
+
         exit()
 
     read_codes = {}
@@ -22,17 +29,20 @@ def main():
         ret, frame = cap.read()
 
         if not ret:
-            print("Can't receive frame (stream end?). Exiting...")
+            logger.info("Can't receive frame (stream end?). Exiting...")
             break
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
         cv.imshow('frame',cv.flip(gray,flipCode=1))
         last_capture = detect_and_decode(qreader, gray, read_codes, last_capture)
 
-    print(read_codes)
+    print(json.dumps(read_codes))
 
     cap.release()
     cv.destroyAllWindows()
+
+    
+    sys.exit(0)
 
 def detect_and_decode(qreader, image, read_codes, last_capture):
     detected = qreader.detect(image=image)
@@ -49,9 +59,9 @@ def detect_and_decode(qreader, image, read_codes, last_capture):
 
     if decoded not in read_codes:
         read_codes[decoded] = decoded
-        print(f"New code detected. Number of read codes: {len(read_codes)}")
+        logger.info(f"New code detected. Number of read codes: {len(read_codes)}")
     else:
-        print("Code already detected")
+        logger.info("Code already detected")
 
     return datetime.now()
 
