1f72eb31dde7a6525af9cf85f3c210fad65f7f0a
bhashimoto <brunothashimoto@gmail.com>
2024-07-11 21:41:15 -0300

    fix framerate issue. Changed qr reading library from Qreader to pyzbar

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import sys
 import logging
 import json
 import cv2 as cv
-from qreader import QReader
-from datetime import datetime
+import pyzbar.pyzbar as pyzbar
 
 logger = logging.getLogger(__name__)
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    last_capture = datetime(2000,1,1)
     cap = cv.VideoCapture(0)
-    cap.set(cv.CAP_PROP_FPS,30)
-    qreader = QReader(model_size='n', min_confidence=0.8)
 
     if not cap.isOpened():
         logger.info("Cannot open camera")
@@ -31,10 +27,9 @@ def main():
         if not ret:
             logger.info("Can't receive frame (stream end?). Exiting...")
             break
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        cv.imshow('frame',cv.flip(gray,flipCode=1))
-        last_capture = detect_and_decode(qreader, gray, read_codes, last_capture)
+        cv.imshow('frame',cv.flip(frame,flipCode=1))
+        detect_and_decode(frame, read_codes)
 
     print(json.dumps(read_codes))
 
@@ -44,27 +39,16 @@ def main():
     
     sys.exit(0)
 
-def detect_and_decode(qreader, image, read_codes, last_capture):
-    detected = qreader.detect(image=image)
-    if len(detected) == 0:
-        return last_capture
-
-    if (datetime.now() - last_capture).total_seconds() < 5.0:
-        return last_capture
+def detect_and_decode(frame, read_codes):
+    decoded = pyzbar.decode(frame)
+    for obj in decoded:
+        url = str(obj.data, "utf-8")
+        if "http" not in url:
+            continue
+        if url not in read_codes:
+            read_codes[url] = url
+            logger.info(f"New code added. Total count: {len(read_codes)}. Code added: {url}")
     
-    decoded = qreader.decode(image, detected[0])
-
-    if not decoded:
-        return last_capture
-
-    if decoded not in read_codes:
-        read_codes[decoded] = decoded
-        logger.info(f"New code detected. Number of read codes: {len(read_codes)}")
-    else:
-        logger.info("Code already detected")
-
-    return datetime.now()
-
 
 if __name__ == "__main__":
     main()
